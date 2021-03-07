d1609c738876588468408ae1d6d5aa7253789750
Jordan Nishimura <jordan.nish@gmail.com>
2021-03-06 16:21:26 -0800

    feat: train SVC model with optimized hyperparameters

--- src/data/.ipynb_checkpoints/make_betting_dataset-checkpoint.py
+++ src/data/.ipynb_checkpoints/make_betting_dataset-checkpoint.py
@@ -1,32 +1,64 @@
 import numpy as np
 import pandas as pd
 
+
 def clean_spread_data(df):
-    df['date'] = pd.to_datetime(df['date'])
-    df['home_team_abbr'] = df['home_team_abbr'].replace({'NY':'NYK',
-                                                                            'GS':'GSW',
-                                                                            'SA':'SAS',
-                                                                            'BK':'BKN',
-                                                                            'NO':'NOP',
-                                                                            'PHO':'PHX'
-                                                                                        }
-                                                                             )
-    df['away_team_abbr'] = df['away_team_abbr'].replace({'NY':'NYK',
-                                                                            'GS':'GSW',
-                                                                            'SA':'SAS',
-                                                                            'BK':'BKN',
-                                                                            'NO':'NOP',
-                                                                            'PHO':'PHX'}
-                                                                           )
 
-    teams = df['home_team_abbr'].unique()
+    abbr_mapping = {'Boston':'BOS', 'Portland':'POR', 
+                'L.A. Lakers':'LAL', 'Brooklyn':'BKN', 
+                'Cleveland':'CLE', 'Toronto':'TOR',
+                'Philadelphia':'PHI', 'Memphis':'MEM',
+                'Minnesota':'MIN', 'New Orleans':'NOP',
+               'Oklahoma City':'OKC', 'Dallas':'DAL', 
+                'San Antonio':'SAS', 'Denver':'DEN', 
+                'Golden State':'GSW', 'L.A. Clippers':'LAC', 
+                'Orlando':'ORL', 'Utah':'UTA', 
+                'Charlotte':'CHA', 'Detroit':'DET',
+               'Miami':'MIA', 'Phoenix':'PHX',
+                'Atlanta':'ATL', 'New York':'NYK', 
+                'Indiana':'IND', 'Chicago':'CHI',
+               'Houston':'HOU', 'Milwaukee':'MIL',
+                'Sacramento':'SAC', 'Washington':'WAS'}
 
-    df = df.sort_values(['date'])
+    df['home_team'] = df['home_team'].replace(abbr_mapping)
+    df['away_team'] = df['away_team'].replace(abbr_mapping)
+    
+    df['away_spread'] = df['away_spread'].str.replace("'", "")
+    df['away_spread'] = df['away_spread'].str.replace("½", ".5")
+    df['away_spread'] = df['away_spread'].str.replace("PK", "+0")
+    df['away_spread'] = df['away_spread'].str.replace("+", "")
+
+    
+    df['home_spread'] = df['home_spread'].str.replace("'", "")
+    df['home_spread'] = df['home_spread'].str.replace("½", ".5")
+    df['home_spread'] = df['home_spread'].str.replace("PK", "+0")
+    df['home_spread'] = df['home_spread'].str.replace("+", "")
+
+
+    away_spreads = df['away_spread'].str.split(',', expand=True)
+    home_spreads = df['home_spread'].str.split(',', expand=True)
+    away_spreads.columns = ['away_spread1', 'away_spread2', 'away_spread3', 'away_spread4']
+    home_spreads.columns = ['home_spread1', 'home_spread2', 'home_spread3', 'home_spread4']
+    
+    full_df = pd.concat([df, away_spreads, home_spreads], axis=1)
+    full_df = full_df.drop(columns=['away_spread', 'home_spread'])
+    
+    columns = full_df.columns[5:]
+    for col in columns:
+        full_df[col] = full_df[col].str.strip()
+        full_df[col] = full_df[col].str[:-4]
+        full_df.loc[full_df[col] == '', col] = np.nan
+        full_df.loc[full_df[col] == '-', col] = np.nan
+        full_df.loc[full_df[col] == '-.', col] = 0
         
-    relevant_betting = df[['date', 'home_team_abbr',  'away_team_abbr',
-                           'home_score', 'away_score', 'spread', 'total']]
-                          
-    return relevant_betting
+    for col in columns:
+        full_df[col] = full_df[col].apply(lambda x: str(x)[:-1] if str(x)[-1] == '-' else x)
+        full_df[col] = full_df[col].astype(float)
+        
+    full_df['away_spread_mode'] = full_df[['away_spread1', 'away_spread2', 'away_spread3', 'away_spread4']].mode(axis=1)[0]
+    full_df['home_spread_mode'] = full_df[['home_spread1', 'home_spread2', 'home_spread3', 'home_spread4']].mode(axis=1)[0]
+
+    return full_df
 
 
 def clean_moneyline_data(df):
